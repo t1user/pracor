99e3ae06da2b33f9f1dcbb6acc0c6fd6cef71f82
Tomek <tomasz2605@gmail.com>
2018-01-19 17:09:36 +0000

    modified fixtures for benefits

---
+++ static_root/js/scripts.js
@@ -0,0 +1,76 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function showMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+/* show searchbar on mobile */
+function showSearch() {
+  var x = document.getElementById("mobile-searchbar");
+    if (x.className === "search-mobile") {
+        x.className += " responsive";
+    } else {
+        x.className = "search-mobile";
+    }
+}
+
+
+/* switch off menu and searchbar on click outside */
+var m = document.querySelector('main')
+m.addEventListener("click", function() {
+  var x = document.getElementById("myTopnav");
+  var y = document.getElementById("mobile-searchbar");
+  if (x.className === "topnav responsive") {
+    x.className = "topnav";
+};
+  if (y.className === "search-mobile responsive") {
+    y.className = "search-mobile";
+};
+}
+)
+
+/* on mobile - expand and collapse menu with add buttons*/
+function listItems() {
+    var x = document.getElementById("actionButton");
+    if (x.className === "action-button") {
+        x.className += " responsive";
+        } else {
+            x.className = "action-button";
+}
+}
+
+/* autocomplete for search form */
+$(function() {
+  $(".auto-search").autocomplete({
+    delay: 150,
+    source: "/search/",
+    minLength: 3,
+    select: function(event, ui) {
+      location.href="/" + ui.item.id;
+}
+});
+});
+
+
+
+$(".auto-position").autocomplete({
+  delay: 150,
+  source: function(request, response) {
+    $.ajax({
+      url:'',
+      dataType: 'json',
+      data:{
+        'term': request.term,
+        'field': this.element['0']['id']
+      },
+      success: function(data) {
+        response(data);
+      }
+    })
+  },
+  minLength: 3,
+});
